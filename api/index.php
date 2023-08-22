0d31551ea40d6f3c002c4f087f3e4c09b12373f4
PerAcc <695856669@qq.com>
2023-08-22 11:46:48 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,12 @@
 <?php
-echo "DearXuan's API by php!";
+$file= __DIR__ . '/..'.$_SERVER["PHP_SELF"];
 
->
+if(file_exists($file))
+{
+   return false;
+}
+else
+{
+    require_once __DIR__ . '/../index.php';
+}
+#echo $_SERVER["PHP_SELF"];
