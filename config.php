a374541c2cf83e8deac63f858785be1a0a6a9993
PerAcc <695856669@qq.com>
2023-08-22 11:57:37 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /*数据库配置*/
 $dbconfig=array(
-	"host" => "localhost", //数据库服务器
-	"port" => 3306, //数据库端口
-	"user" => "", //数据库用户名
-	"pwd" => "", //数据库密码
-	"dbname" => "", //数据库名
+	"host" => "containers-us-west-194.railway.app", //数据库服务器
+	"port" => 7799, //数据库端口
+	"user" => "root", //数据库用户名
+	"pwd" => "hVz5t28urjnVFmYhHpHS", //数据库密码
+	"dbname" => "railway", //数据库名
 );
 ?>
